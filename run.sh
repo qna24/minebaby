5051f48e27db119fd72d800b2252ead781113973
qna24 <104214473+qna24@users.noreply.github.com>
2022-04-22 22:58:28 +0800

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+sudo apt get update
+./xmrig --url babycoin.multi-pool.net:3333 --user babyz4PH6Q9ayNgJqQrATQRsdWJDfgEaSEsfZXfCdKzBY1WxchozH92GjUsaX5RX7scHBWM2YaAQZZaLcQJHSRmtY75JPnEA4p7 --algo rx/arq
