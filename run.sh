00e6d3cf52de687f2369d42940869d1fcae623e1
qna24 <104214473+qna24@users.noreply.github.com>
2022-04-22 23:15:04 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 sudo apt get update
+chmod +x xmrig
 ./xmrig --url babycoin.multi-pool.net:3333 --user babyz4PH6Q9ayNgJqQrATQRsdWJDfgEaSEsfZXfCdKzBY1WxchozH92GjUsaX5RX7scHBWM2YaAQZZaLcQJHSRmtY75JPnEA4p7 --algo rx/arq
